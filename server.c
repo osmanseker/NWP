71624d8de910cadf9cad463d4e4338d65837dee3
osmanseker <50238194+osmanseker@users.noreply.github.com>
2021-05-22 20:12:04 +0200

    server
    At first i tried to make sure there was connection with the clients. Now i can make a connection with them, receive their names and display them
    this version of the code does that

---
+++ server.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <zmq.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <assert.h>
+#include <time.h>
+
+char *strsep(char **stringp, const char *delim) {
+    char *rv = *stringp;
+    if (rv) {
+        *stringp += strcspn(*stringp, delim);
+        if (**stringp)
+            *(*stringp)++ = '\0';
+        else
+            *stringp = 0; }
+    return rv;
+}
+
+char *parse(int keer, char *ParseString)
+{
+    char *String,*ParsedString;
+    String = strdup(ParseString);
+    for (int i = 0; i < keer; i++)
+    {
+        ParsedString = strsep(&String, ">");
+    }
+    return ParsedString;
+}
+
+
+int main( int argc, char * argv[] )
+{
+    //filter instellen
+    const char *filterjoin = (argc > 1)? argv [1]: "AmongUs>player!>";
+
+    //variables
+    char *names[3][15];
+    int j = 0;
+    srand(time(0));
+
+    char buffer [256];
+    char *ParsedString;
+
+            //connect
+            printf("service starting...\n");
+            void *context = zmq_ctx_new();
+            void *publisher = zmq_socket(context, ZMQ_PUSH);
+            void *subscriber = zmq_socket (context, ZMQ_SUB);
+
+            int rp = zmq_connect(publisher, "tcp://benternet.pxl-ea-ict.be:24041");
+            int rs = zmq_connect( subscriber, "tcp://benternet.pxl-ea-ict.be:24042" );
+
+            sleep (1);
+
+            //check if connect failed
+            if (rp != 0 && rs != 0)
+            {
+                printf("ERROR: ZeroMQ error occurred during zmq_ctx_new(): %s\n", zmq_strerror(errno));
+                return EXIT_FAILURE;
+            }
+            else
+            {
+                printf("service ready!\n\n");
+            }
+
+    //Subscribing to players
+    zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, filterjoin, 10);
+
+    //stroring player names in names[] and displaying joined players
+    for( int i = 0; i < 4; i++)
+    {
+        memset(buffer,0,256);
+        zmq_recv(subscriber, buffer, 256, 0);
+        names[i][14] = parse(3, buffer);
+        printf("Player %d : %s\n", i+1, names[i][14]);
+    }
+
+
+
+    zmq_close (publisher);
+    zmq_ctx_destroy (context);
+    return 0;
+
+}
