d0a0117a9163d7f21d5580d1a5db8ba4b2b2eba4
osmanseker <50238194+osmanseker@users.noreply.github.com>
2021-05-30 15:49:25 +0200

    Update server.c
    laatste versie (ontvangen van namen + votes(maar votes fout) + imposter selecteren(lukt niet)

--- server.c
+++ server.c
@@ -34,18 +34,18 @@ int main( int argc, char * argv[] )
     //filter instellen
     const char *filterjoin = (argc > 1)? argv [1]: "AmongUs>player!>";
     const char *filtervote = (argc > 1)? argv [1]: "AmongUs>vote!>";
+    const char *filterimposter = (argc > 1)? argv [1]: "AmongUs>imposter!>";
 
     //variables
     int votes[10] = {0};
-    char *names[10][15];
+    int votecount = 0;    
     int players = 10;
     int rounds = 9;
     int opsomming = 1;
-    char opsommingS[10];
-    int j = 0;
     srand(time(0));
 
-
+    char opsommingS[10];
+    char names[10][100];
     char sentnames[16] = "AmongUs>Player?>";
     char sentall[256];
     char buffer [256];
@@ -82,19 +82,21 @@ int main( int argc, char * argv[] )
     {
         memset(buffer,0,256);
         zmq_recv(subscriber, buffer, 256, 0);
-        names[i][14] = parse(3, buffer);
-        printf("Player %d : %s\n", i+1, names[i][14]);
+        //names[i] = parse(3, buffer);
+        strcpy(names[i],parse(3, buffer) );
+        printf("Player %d : %s\n", i+1, names[i]);
     }
 
     //print list of players to all clients
+    buffer[0] = '\0';
+    memset(sentall,0,256);
     strcat(sentall,"These are the players that joined\n\n");
     for (int i = 0; i < 4; i++)
     {
 
         sprintf(opsommingS, "%d. ",i+1);
         strcat(sentall, opsommingS);
-        //strcat(sentall, " ");
-        strcat(sentall,names[i][14]);
+        strcat(sentall,names[i]);
         strcat(sentall, "\n");
 
     }
@@ -103,6 +105,27 @@ int main( int argc, char * argv[] )
     printf("\nAll players joined\n\n");
 
     sleep(1);
+/*
+    //subscribe to imposters and selecting 2 random imposters
+    zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, filterimposter, 18);
+    printf("choosing 2 random imposters...\n\n");
+    int j = 0;
+    int i = rand()%4;
+        printf("%s", names[i]);
+        do
+        {
+            j= rand()%10;
+        }
+        while(i == j);
+        printf("%s\n", names[j]);
+
+        printf("imposter is gekozen\n\n");
+        zmq_send(publisher, "AmongUs>imposter!>You are imposter!", 35,0);
+//    }
+*/
+
+
+
 
     //voting system for crewmembers
     printf("Crewmembers are voting...\n\n");
@@ -118,18 +141,44 @@ int main( int argc, char * argv[] )
         zmq_recv(subscriber, buffer, 256, 0);
         ParsedString = parse(3, buffer);
         printf("%s\n", buffer);
-        //votes[i] = atoi(ParsedString);;
+        votes[i] = atoi(ParsedString);;
         //printf("Player %s voted for %d\n", names[i][14], votes[i]);
+
+        votes[i-1] = votes [i-1]+1;
+
     }
-    printf("debug print after votes came in");
+    printf("debug print after votes came in + count\n\n");
 
+    for (int i = 0;i<1;i++)
+    {
+        printf("%s heeft %d votes\n", names[i], votes[i]);
+    }
 
-
-
+    int temp = 0;
+    int most_votes = votes[0];
+    for (int i = 0; i < 4 ; i++)
+    {
+        if(most_votes < votes[i])
+        {
+            most_votes = votes[i];
+            temp = i;
+        }
+    }
+    printf("\n%s\n", votes[temp]);
+//
+//    for(int i = temp; i < rounds;i++)
+//    {
+//       strcpy(names[i], names[i+1]);
 //    }
+
+
+
+
+
     zmq_close (subscriber);
     zmq_close (publisher);
     zmq_ctx_destroy (context);
     return 0;
+
 
 }
